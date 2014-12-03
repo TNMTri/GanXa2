3027a2e768bc27bd3822f081b4799cb14c36f3a2
TNMTri <tri.tran@softersource.com>
2014-12-03 09:12:17 +0700

    Sửa lỗi trong edit_store.

--- routes/edit_store.js
+++ routes/edit_store.js
@@ -33,7 +33,16 @@ router.post('/', function (req, res) {
 
     var store_id = req.param('id');
     var store_name = req.body.txtStoreName;
-    var address = req.body.txtAddress; //Phải chia nhỏ ra nữa.
+
+    var count_address = 1; //req.body.txtCountAddress;
+    var address = [];
+    for (var i = 1; i < count_address.length; i++) {
+        var city = req.body.txtCity + i;
+        var district = req.body.txtDistrict + i;
+        var street = req.body.txtStreet + i;
+        address.push({"city": city, "district": district, "street": street});
+    }
+
     var latitude = req.body.txtLatitude;
     var longitude = req.body.txtLongitude;
     var phone = req.body.txtPhone;
@@ -44,39 +53,47 @@ router.post('/', function (req, res) {
     var fanpage = req.body.txtFanpage;
 
     //Cover:
-    if (typeof req.files.ulfCover === 'undefined') {
-        var cover = cover;
-    } else {
+    var cover_new = cover;
+    if (typeof req.files.ulfCover != 'undefined') {
         var cover_upload_path = req.files.ulfCover.path;
         var cover_save_path = "public/images/" + req.files.ulfCover.name;
-        im.resize({
-            srcPath: cover_upload_path,
-            dstPath: cover_save_path,
-            width: 800
-        }, function (err, stdout, stderr) {
-            console.log('Resize cover success.');
+        im.identify(cover_upload_path, function (cover_error, cover_features) {
+            if (cover_features) {
+                im.resize({
+                    srcPath: cover_upload_path,
+                    dstPath: cover_save_path,
+                    width: cover_features.width / 2,
+                    height: cover_features.height / 2
+                }, function (err, stdout, stderr) {
+                });
+            }
         });
         cover_save_path = ".." + req.files.ulfCover.path.replace("public", "");
-        cover = cover_save_path;
+        cover_new = cover_save_path;
     }
+console.log(cover_new);
     //Logo:
-    if (typeof req.files.ulfLogo === 'undefined') {
-        var logo = logo;
-    } else {
+    var logo_new = logo;
+    if (typeof req.files.ulfLogo != 'undefined') {
         var logo_upload_path = req.files.ulfLogo.path;
         var logo_save_path = "public/images/" + req.files.ulfLogo.name;
-        im.resize({
-            srcPath: logo_upload_path,
-            dstPath: logo_save_path,
-            width: 250
-        }, function (err, stdout, stderr) {
-            console.log('Resize logo success.');
+        im.identify(logo_upload_path, function (logo_error, logo_features) {
+            if (logo_features) {
+                im.resize({
+                    srcPath: logo_upload_path,
+                    dstPath: logo_save_path,
+                    width: logo_features.width / 2,
+                    height: logo_features.height / 2
+                }, function (err, stdout, stderr) {
+                });
+            }
         });
         logo_save_path = ".." + req.files.ulfLogo.path.replace("public", "");
-        cover = logo_save_path;
+        logo_new = logo_save_path;
     }
+    console.log(logo_new);
 
-    store_schema.store.update({_id: store_id}, {$set: {store_name: store_name, address: address, latitude: latitude, longitude: longitude, phone: phone, description: description, industry: industry, hours_of_work: hours_of_work, cover: cover, logo: logo, website: website, fanpage: fanpage}}, function (err, result) {
+    store_schema.store.update({_id: store_id}, {$set: {store_name: store_name, address: address, latitude: latitude, longitude: longitude, phone: phone, description: description, industry: industry, hours_of_work: hours_of_work, cover: cover_new, logo: logo_new, website: website, fanpage: fanpage}}, function (error, result) {
         if (!error && result) {
             store_schema.store.find(function (store_error, store_array) {
                 res.render('home', {store_array: store_array});
