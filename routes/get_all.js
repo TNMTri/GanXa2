2def5bc801d080081ece98920cc9c27f8d7177ad
TNMTri <tri.tran@softersource.com>
2014-12-05 09:40:51 +0700

    Fix Style.css

--- routes/get_all.js
+++ routes/get_all.js
@@ -1,10 +1,39 @@
-var get_all = {
-    get_all_data: function () {
-        console.log("bla bla");
-    }
-};
+var express = require('express');
+var router = express.Router();
 
-module.exports = function (router) {
-    router.get('/', get_all.get_all_data());
-    return router;
-};
+var industry_schema = require('../models/industry_schema');
+var store_schema = require('../models/store_schema');
+var product_schema = require('../models/product_schema');
+
+router.get('/', function (req, res) {
+
+    var query_industry = industry_schema.industry.find({});
+    query_industry.sort({date: -1});
+    query_industry.exec(function (industry_error, industry_array) {
+        if(industry_array && industry_array.length > 0){
+            req.session.industry_array = industry_array;
+        }
+    });
+
+    var query_store = store_schema.store.find({});
+    query_store.limit(8);
+    query_store.sort({date: -1});
+    query_store.exec(function (store_error, store_array) {
+        if (store_array && store_array.length > 0) {
+            req.session.store_array = store_array;
+        } else {
+            console.log(store_error);
+        }
+    })
+
+    var query_product = product_schema.product.find({});
+    query_product.sort({date: -1});
+    query_product.exec(function (product_error, peoduct_array) {
+        if(peoduct_array && peoduct_array.length > 0){
+            req.session.industry_array = peoduct_array;
+        }
+    })
+
+});
+
+module.exports = router;
