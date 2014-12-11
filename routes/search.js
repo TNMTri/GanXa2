94bbe4dd84a4cd2ab5ea678fe50bb2b3724de5ac
Beto Zugi <betokutin@gmail.com>
2014-12-11 14:23:41 +0700

    Thêm trang industry + insert industry bằng poup trong trang industry

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var industry_schema = require('../models/industry_schema');
 var store_schema = require('../models/store_schema');
 var product_schema = require('../models/product_schema');
 router.get('/', function (req, res) {
@@ -28,5 +29,23 @@ router.post('/', function (req, res) {
         });
     }
 });
-
+router.get('/', function (req, res) {
+    var type = req.param('type');
+    /*var u = require('unidecode');
+     var x = u('ấ');
+     console.log(x);*/
+    if (!req.param('type')) {
+        store_schema.store.find(function (store_error, store_array) {
+            industry_schema.industry.find(function (industry_error, industry_array) {
+                req.session.store_array = store_array;
+                req.session.industry_array = industry_array;
+                res.render('search', {store_array: store_array, industry_array: industry_array,notification : "search industry"});
+            });
+        });
+    } else {
+        store_schema.store.find({store_category: {$in: [req.param("type")]}}, function (store_error, store_array) {
+            res.render('search', {category_array: req.session.category_array, store_array: store_array});
+        });
+    }
+});
 module.exports = router;
